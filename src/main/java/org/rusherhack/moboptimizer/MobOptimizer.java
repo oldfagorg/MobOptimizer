87b07739507e1cbe6cc0eebb3a2122b2f7992ea8
John <43681932+John200410@users.noreply.github.com>
2020-04-25 22:29:57 -0400

    Update MobOptimizer.java

--- src/main/java/org/rusherhack/moboptimizer/MobOptimizer.java
+++ src/main/java/org/rusherhack/moboptimizer/MobOptimizer.java
@@ -2,22 +2,13 @@ package org.rusherhack.moboptimizer;
 
 import org.bukkit.Bukkit;
 import org.bukkit.World;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Monster;
+import org.bukkit.entity.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
- * this is a plugin i wrote in about 25 minutes to help with the lag on oldfag.org
- * works better than i expected
- *
  * @author John200410 4/23/2020 for OptimizeMobs
  */
 public class MobOptimizer extends JavaPlugin {
-
-	@Override
-	public void onDisable() {
-	}
 
 	@Override
 	public void onEnable() {
@@ -42,7 +33,6 @@ public class MobOptimizer extends JavaPlugin {
 	private boolean shouldLimitEntity(LivingEntity entity) {
 		switch (entity.getType()) {
 			case BAT:
-			case SNOWMAN:
 			case RABBIT:
 			case POLAR_BEAR:
 			case ENDERMITE:
@@ -50,12 +40,17 @@ public class MobOptimizer extends JavaPlugin {
 				return true;
 			default:
 		}
-		if(entity.fromMobSpawner() && getPlayerCount() < 100) {
+
+		if(/* entity has nametag */entity.getCustomName() != null || entity instanceof Wolf || entity instanceof Parrot || entity.fromMobSpawner() && getPlayerCount() < 110) {
 			return false;
+		} else if(entity instanceof Skeleton) {
+			return isEntityNearPlayer(entity, 10);
+		} else if(!isEntityNearPlayer(entity, 40)) {
+			return true;
 		} else if(entity instanceof Monster) {
 			return getTps() < 10 || getPlayerCount() > 90;
 		} else {
-			return getTps() < 6 || getPlayerCount() > 110;
+			return getTps() < 6 || getPlayerCount() > 120;
 		}
 	}
 
@@ -65,6 +60,15 @@ public class MobOptimizer extends JavaPlugin {
 
 	private int getTps() {
 		return (int) Bukkit.getServer().getTPS()[0];
+	}
+
+	private boolean isEntityNearPlayer(LivingEntity entity, double distance) {
+		for (Player player : Bukkit.getOnlinePlayers()) {
+			if(player.getNearbyEntities(distance, distance, distance).contains(entity)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
